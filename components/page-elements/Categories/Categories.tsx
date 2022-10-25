c680c9c9d45a72c7c1a33e386ae4c16641d1c72c
elminhoemmanuel <amodue15@gmail.com>
2022-10-25 15:25:10 +0100

    fixed responsiveness bug on categories slider

--- components/page-elements/Categories/Categories.tsx
+++ components/page-elements/Categories/Categories.tsx
@@ -5,7 +5,7 @@ import CarouselSmall from '../CarouselSmall'
 const Categories = () => {
   return (
     <div className='py-6 px-6 md:px-8 lg:px-12 grid grid-cols-12 gap-3'>
-        <div className='col-span-12 md:col-span-11'>
+        <div className='col-span-12 md:col-span-10 lg:col-span-11'>
             <CarouselSmall />
             <CarouselLarge />
         </div>
